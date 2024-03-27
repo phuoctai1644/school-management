6e16447d057f07bb9fb79a5b2d9c02207e449420
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-03-27 22:24:24 +0700

    feat: handle add new student

--- student/views.py
+++ student/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView
 from student.models import Student
+from django.urls import reverse
 
 
 class StudentListView(ListView):
     template_name = 'list.html'
     model = Student
     context_object_name = 'students'
+
+
+def student_list(request):
+    student_obj = Student.objects.all()
+
+    if request.method == 'GET':
+        return render(request, 'list.html', {'students': student_obj})
+    elif request.method == 'POST':
+        form_data = {}
+        for key in request.POST:
+            form_data[key] = request.POST[key]
+        new_student = Student(
+            name=form_data['name'],
+            age=form_data['age'],
+            sex=form_data['sex'],
+            avatar=request.FILES['avatar']
+        )
+        new_student.save()
+        return HttpResponseRedirect(reverse('student:List'))
