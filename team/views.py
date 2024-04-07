9f41bdcb8083a85d224516ccf15407a24f358b3f
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2024-04-07 09:58:18 +0700

    chore: handle model + html base

--- team/views.py
+++ team/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Team
 
-# Create your views here.
+
+def team_list(request):
+    team_obj = Team.objects.all()
+    return render(request, 'team-list.html', {'teams': team_obj})
